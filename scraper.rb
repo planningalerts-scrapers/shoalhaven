1790636f78b4fd5e2c4da4390d5c806d841dc436
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 12:50:14 +1000

    No need to save comment_url

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,6 @@ require 'date'
 url = "http://doc.shoalhaven.nsw.gov.au/RSS/SCCRSS.aspx?ID=OpenApps"
 doc = Nokogiri::XML(open(url))
 
-comment_url = 'mailto:council@shoalhaven.nsw.gov.au'
-
 das = doc.xpath('//channel/item').collect do |item|
   item = Nokogiri::XML(item.to_xml)
   table = Nokogiri::HTML(item.at_xpath('//description').inner_text)
@@ -20,14 +18,12 @@ das = doc.xpath('//channel/item').collect do |item|
   page_info['description'] = item.at_xpath('//title').inner_text.split[2..-1].join(' ')
   page_info['date_received'] = Date.strptime(table_values['Date received:'], '%d %B %Y').to_s
   page_info['address'] = table_values['Address:'] + ', NSW'
-  page_info['on_notice_to'] = Date.strptime(table_values['Submissions close:'], '%d %B %Y').to_s 
+  page_info['on_notice_to'] = Date.strptime(table_values['Submissions close:'], '%d %B %Y').to_s
   page_info['date_scraped'] = Date.today.to_s
-  page_info['comment_url'] = comment_url
-  
+
   page_info
 end
 
 das.each do |record|
   ScraperWiki.save_sqlite(['council_reference'], record)
 end
-
